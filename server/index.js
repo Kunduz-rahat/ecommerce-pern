6efea434cc6bf90c2c8c9a9519e682a5e79752ca
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-15 12:58:08 +0600

    add routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const cors = require('cors')
 require('dotenv').config()
 const sequelize=require('./db')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
+const models = require('./models/models');
+const router = require('./routes/index');
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
 
 const start = async()=>{
 	try{
