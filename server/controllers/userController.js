d309da7e066d4cb36c0c600e0709fe5ab5e0a6ee
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-16 14:23:13 +0600

    adding a middleware to check the user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,8 +44,9 @@ class UserController{
 	 const token = generateJwt(user.id, user.email, user.role)
 	 return res.json({token})
 	}
-	 async check(reg, res, next){
-    
+	 async check(req, res, next){
+    const token = generateJwt(req.user.id, req.user.email, req.user.role)
+	 return res.json({token})
 	 }
 }
  module.exports = new UserController() 
