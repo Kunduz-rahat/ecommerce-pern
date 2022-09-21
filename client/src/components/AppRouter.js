6783f081ee20a4a97962e4b25aeb170066730a1c
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-21 11:56:32 +0600

    add navbar

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {Routes, Route, Navigate} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
 import Shop from '../pages/Shop';
 import {Context} from '../index'
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import {Container} from 'react-bootstrap'
 export default function Auth() {
   return (
-	 <div>Auth</div>
+	 <Container>Auth</Container>
   )
 }
