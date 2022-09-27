342ffe8c778305f698986ee207b1883df87aaeb3
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-27 21:50:58 +0600

    adding functional for device_brand

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Modal, Button, Form , Dropdown, Row, Col} from 'react-bootstrap';
+import {Context} from "../../index"
 
 export default function CreateBrand({show, onHide}) {
+	const {device} = useContext(Context)
+	const [info, setInfo] = useState([])
+	const addInfo =()=>{
+		setInfo([...info, {title:'', description:'', number: Date.now()}])
+	}
+	const removeInfo =(number)=>{
+		setInfo(info.filter(i=>i.number !== number))
+	}
   return (
 	<Modal
 	show={show}
@@ -17,7 +26,47 @@ export default function CreateBrand({show, onHide}) {
 	</Modal.Header>
 	<Modal.Body>
 	  <Form>
-		<Form.Control placeholder={"Введите название бренда"}/>
+		{/* <Form.Control placeholder={"Введите название бренда"}/> */}
+		<Dropdown className='mt-3'>
+			<Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+			<Dropdown.Menu>
+				{device.types.map(type=>
+				<Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+				)}
+			</Dropdown.Menu>
+		</Dropdown>
+		<Dropdown className='mt-3'>
+			<Dropdown.Toggle>Выберите бренд</Dropdown.Toggle>
+			<Dropdown.Menu>
+				{device.brands.map(brand=>
+				<Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+				)}
+			</Dropdown.Menu>
+		</Dropdown>
+		<Form.Control placeholder='Введите название устройства' className='mt-3'/>
+		<Form.Control type='number' placeholder='Введите стоимость устройства' className='mt-3'/>
+		
+		<Form.Control type='file'  className='mt-3'/>
+		<br/>
+		<Button onClick={addInfo}>Добавить новое свойство</Button>
+		{
+			info.map(item =>
+			<Row className='mt-3' key={item.number}>
+         <Col md={4}>
+			<Form.Control
+				placeholder='Введите название свойства'
+			/>
+			</Col>
+			<Col md={4}>
+			<Form.Control
+				placeholder='Введите описание свойства'
+			/>
+</Col>
+ <Col md={4}>
+<Button onClick={()=>removeInfo(item.number)}>Удалить</Button>
+</Col>
+			</Row>)
+		}
 	  </Form>
 	</Modal.Body>
 	<Modal.Footer>
