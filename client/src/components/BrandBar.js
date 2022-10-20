80b2da754ff407724042b5458670edcc1a202b2c
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-10-20 20:23:47 +0600

    fixed a bug

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -4,23 +4,26 @@ import { Context } from '../index';
 import {Row, Card, Col} from 'react-bootstrap'
 
 
-const BrandBar=observer(()=> {
+const BrandBar = observer(() => {
 	const {device} = useContext(Context)
-  return (
-	 <Row className="d-flex">
-	 {device.brands.map(brand=>
-	 <Col >
-	 <Card key={brand.id} className="p-3"
-	 onClick={()=> device.setSelectedBrand(brand)}
-	 border={brand.id===device.selectedBrand.id?"danger" : "light"}
-	 style={{cursor:"pointer"}}
-	 >
-		{brand.name}
-	 </Card>
-	 </Col>
-	
-	 )}
-	 </Row>
-  )
-})
+
+	return (
+		 <Row className="d-flex">
+			  {device.brands.map(brand =>
+			  <Col>
+			  <Card
+						 style={{cursor:'pointer'}}
+						 key={brand.id}
+						 className="p-3"
+						 onClick={() => device.setSelectedBrand(brand)}
+						 border={brand.id === device.selectedBrand.id ? 'danger' : 'light'}
+					>
+						 {brand.name}
+					</Card>
+			  </Col>
+				
+			  )}
+		 </Row>
+	);
+});
 export default BrandBar
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -4,24 +4,39 @@ import CreateBrand from '../components/modals/CreateBrand';
 import CreateDevice from '../components/modals/CreateDevice';
 import CreateType from '../components/modals/CreateType';
 
-export default function Admin() {
+const Admin = () => {
   const [brandVisible, setBrandVisible] = useState(false)
   const [typeVisible, setTypeVisible] = useState(false)
   const [deviceVisible, setDeviceVisible] = useState(false)
+
   return (
-	 <Container className='d-flex flex-column '>
-   <Button onClick={()=> setTypeVisible(true)}>
-     Добавить тип
-   </Button>
-   <Button onClick={()=> setBrandVisible(true)}>
-     Добавить бренд
-   </Button>
-   <Button onClick={()=> setDeviceVisible(true)}>
-     Добавить устройство
-   </Button>
-   <CreateType show={typeVisible} onHide={()=> setTypeVisible(false)}/>
-   <CreateBrand show={brandVisible} onHide={()=> setBrandVisible(false)}/>
-   <CreateDevice show={deviceVisible} onHide={()=> setDeviceVisible(false)}/>
-   </Container>
-  )
-}
+      <Container className="d-flex flex-column">
+          <Button
+              variant={"outline-dark"}
+              className="mt-4 p-2"
+              onClick={() => setTypeVisible(true)}
+          >
+              Добавить тип
+          </Button>
+          <Button
+              variant={"outline-dark"}
+              className="mt-4 p-2"
+              onClick={() => setBrandVisible(true)}
+          >
+              Добавить бренд
+          </Button>
+          <Button
+              variant={"outline-dark"}
+              className="mt-4 p-2"
+              onClick={() => setDeviceVisible(true)}
+          >
+              Добавить устройство
+          </Button>
+          <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+          <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
+          <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+      </Container>
+  );
+};
+
+export default Admin;
